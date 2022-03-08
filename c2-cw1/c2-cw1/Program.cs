99c3b2f7902ef0115a0d6b7040d226929c3e725b
Dhary01 <Dharym@hotmail.com>
2022-03-08 18:06:32 +0300

    I used multiple stuff you taught me such as string int console.write line etc...

---
+++ c2-cw1/c2-cw1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "Mario";
+
+        int Characterspeed = 3;
+
+            int age = 16;
+
+            string superpower1 = "Fly";
+
+            string superpower2 = "invisibility";
+
+            Console.WriteLine("my name is {0} my speed is {1} my age is {2} my superpowers are {3} and {4}", CharacterName, Characterspeed, age, superpower1, superpower2);
+        }
+    }
+    //string name int maxHP; double characterSpeed; bool isBoss
+}
+
+    
+       
